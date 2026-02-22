b1058584092acb30df8fd686cb654463f0e47c79
Filip Ivanović <filipivanovic59@gmail.com>
2026-02-22 08:16:07 +0100

    props

--- src/index.js
+++ src/index.js
@@ -67,9 +67,28 @@ function Header() {
 function Menu() {
   return (
     <main className="menu">
-      <h1>Our Menu</h1>
-      <Pizza />
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
   );
 }
 
@@ -87,16 +106,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()} We are currently open
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
